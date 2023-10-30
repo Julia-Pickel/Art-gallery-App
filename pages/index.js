3161350bc7927d65db72ca73887926b7b7ba1e2b
Julia Pickel <juliapickel@gmx.de>
2023-10-30 15:35:56 +0100

    created ArtPieces folder

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,27 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const URL = "https://example-apis.vercel.app/api/art";
+  const fetcher = (...args) =>
+    fetch(...args).then((response) => response.json());
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>There is an Error. Try again later.</h1>;
+  }
+
+  // if (data) {
+  //   console.log(data);
+  // }
+
+  // console.log("Loading:", isLoading);
+  // console.log("Error:", error);
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
