b4a101fbdce03a819cb81088686453bc1eacc196
Julia Pickel <juliapickel@gmx.de>
2023-10-31 10:27:34 +0100

    UserStory1 - task completed

--- components/ArtPiecePreview/index.js
+++ components/ArtPiecePreview/index.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 export default function ArtPiecePreview({ image, title, artist }) {
   return (
     <li>
-      <Image alt="" src={image} width="300" height="200">
-        {image}
-      </Image>
+      <Image src={image} alt={title} width={200} height={200} />
       <p>
-        {artist}:{title}
+        <strong>{artist}: </strong>
+        {title}
       </p>
     </li>
   );
 }
+
+// Image is missing required "alt" property.
+// Please add Alternative Text to describe the image for screen readers and search engines.
--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,21 +1,16 @@
 import ArtPiecePreview from "@/components/ArtPiecePreview";
 
 export default function ArtPieces({ pieces }) {
-  const createPiecesList = pieces.map((piece) => {
-    return (
-      <>
-        <ArtPiecePreview
-          image={piece.imageSource}
-          title={piece.title}
-          artist={piece.artist}
-        />
-      </>
-    );
-  });
-
   return (
-    <>
-      <ul>{createPiecesList}</ul>
-    </>
+    <ul>
+      {pieces.map(({ slug, name, imageSource, artist }) => (
+        <ArtPiecePreview
+          key={slug}
+          image={imageSource}
+          title={name}
+          artist={artist}
+        ></ArtPiecePreview>
+      ))}
+    </ul>
   );
 }
